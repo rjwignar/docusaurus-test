361ae7d76fcb4b350c56a325e0de4c3ab4751fdb
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2023-10-31 01:06:25 -0400

    added deploymentBranch variable

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'rjwignar', // Usually your GitHub org/user name.
   projectName: 'docusaurus-test.github.io', // Usually your repo name.
+  deploymentBranch: 'deployment',
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
